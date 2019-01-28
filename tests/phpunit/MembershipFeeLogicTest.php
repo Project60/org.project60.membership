ef556d41ffbf3f8529537328f0c1d7d32b7332d9
B. Endres <endres@systopia.de>
2019-01-28 09:12:28 +0100

    [#28] added tests (WIP)

---
+++ tests/phpunit/MembershipFeeLogicTest.php
@@ -0,0 +1,87 @@
+<?php
+/*-------------------------------------------------------+
+| Project 60 - Membership Extension                      |
+| Copyright (C) 2018 SYSTOPIA                            |
+| Author: B. Endres (endres -at- systopia.de)            |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Membership_ExtensionUtil as E;
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
+require_once 'MembershipTestBase.php';
+
+/**
+ * Test MembershipFeeLogic class
+ *
+ * @group headless
+ */
+class MembershipFeeLogicTest extends MembershipTestBase  {
+
+  public function setUp() {
+    parent::setUp();
+  }
+
+
+  public function tearDown() {
+    parent::tearDown();
+  }
+
+
+  /**
+   * Test the align function
+   */
+  public function testUnitDateDiff() {
+    $logic = new CRM_Membership_MembershipFeeLogic(['time_unit' => 'month']);
+    $this->assertEquals(0, $logic->getDateUnitDiff('2019-01-01', '2019-01-01'));
+    $this->assertEquals(1, $logic->getDateUnitDiff('2019-01-01', '2019-01-02'));
+    $this->assertEquals(1, $logic->getDateUnitDiff('2019-01-01', '2019-01-31'));
+    $this->assertEquals(1, $logic->getDateUnitDiff('2019-01-01', '2019-02-01'));
+    $this->assertEquals(2, $logic->getDateUnitDiff('2019-01-01', '2019-02-02'));
+  }
+
+    /**
+   * Test the align function
+   */
+  public function testAlignDate() {
+    $logic = new CRM_Membership_MembershipFeeLogic(['time_unit' => 'month']);
+    $this->assertEquals('2019-01-01', $logic->alignDate('2019-01-14', FALSE));
+    $this->assertEquals('2019-01-01', $logic->alignDate('2019-01-01', FALSE));
+    $this->assertEquals('2019-01-01', $logic->alignDate('2019-01-31', FALSE));
+    $this->assertEquals('2019-01-31', $logic->alignDate('2019-01-14', TRUE));
+    $this->assertEquals('2019-01-31', $logic->alignDate('2019-01-01', TRUE));
+    $this->assertEquals('2019-01-31', $logic->alignDate('2019-01-31', TRUE));
+
+    $this->assertEquals('2019-02-01', $logic->alignDate('2019-01-31', TRUE, TRUE));
+    $this->assertEquals('2018-12-31', $logic->alignDate('2019-01-31', FALSE, TRUE));
+
+
+    $logic = new CRM_Membership_MembershipFeeLogic(['time_unit' => 'year']);
+    $this->assertEquals('2019-01-01', $logic->alignDate('2019-01-14', FALSE));
+    $this->assertEquals('2019-01-01', $logic->alignDate('2019-02-01', FALSE));
+    $this->assertEquals('2019-01-01', $logic->alignDate('2019-03-31', FALSE));
+    $this->assertEquals('2019-12-31', $logic->alignDate('2019-04-14', TRUE));
+    $this->assertEquals('2019-12-31', $logic->alignDate('2019-05-01', TRUE));
+    $this->assertEquals('2019-12-31', $logic->alignDate('2019-06-31', TRUE));
+
+    $logic = new CRM_Membership_MembershipFeeLogic(['time_unit' => 'week']);
+    $this->assertEquals('2019-01-14', $logic->alignDate('2019-01-14', FALSE));
+    $this->assertEquals('2019-01-14', $logic->alignDate('2019-01-16', FALSE));
+    $this->assertEquals('2019-01-14', $logic->alignDate('2019-01-20', FALSE));
+
+    $this->assertEquals('2019-01-20', $logic->alignDate('2019-01-14', TRUE));
+    $this->assertEquals('2019-01-20', $logic->alignDate('2019-01-14', TRUE));
+    $this->assertEquals('2019-01-20', $logic->alignDate('2019-01-14', TRUE));
+  }
+
+}
---
+++ tests/phpunit/MembershipTestBase.php
@@ -0,0 +1,45 @@
+<?php
+/*-------------------------------------------------------+
+| Project 60 - Membership Extension                      |
+| Copyright (C) 2018 SYSTOPIA                            |
+| Author: B. Endres (endres -at- systopia.de)            |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Membership_ExtensionUtil as E;
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
+/**
+ * Test MembershipFeeLogic class
+ *
+ * @group headless
+ */
+class MembershipTestBase extends \PHPUnit_Framework_TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+
+  public function setUpHeadless() {
+    // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+    // See: https://docs.civicrm.org/dev/en/latest/testing/phpunit/#civitest
+    return \Civi\Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
+  public function setUp() {
+    parent::setUp();
+  }
+
+  public function tearDown() {
+    parent::tearDown();
+  }
+
+}
